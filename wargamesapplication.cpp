f202cf53f4e2d4a7cb5485443a40d5b06148ac2f
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2015-11-29 16:29:58 -0800

    Reinstates SSL support check..

--- wargamesapplication.cpp
+++ wargamesapplication.cpp
@@ -10,12 +10,12 @@ WarGamesApplication::WarGamesApplication(int &argc, char **argv) :
     QCoreApplication::setApplicationName("WarGames");
     QCoreApplication::setApplicationVersion("0.0.0.1");
 
-//    if (!QSslSocket::supportsSsl()) {
-//        QMessageBox::critical(NULL,
-//                              tr("Missing SSL Support"),
-//                              tr("Please install OpenSSL and try again."));
-//        QTimer::singleShot(0, this, SLOT(quit()));
-//        return;
-//    }
+    if (!QSslSocket::supportsSsl()) {
+        QMessageBox::critical(NULL,
+                              tr("Missing SSL Support"),
+                              tr("Please install OpenSSL and try again."));
+        QTimer::singleShot(0, this, SLOT(quit()));
+        return;
+    }
 }
 
