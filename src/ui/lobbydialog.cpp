736dffe31e013caf0f70a3920f4deff9922ebc53
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-05-27 13:38:41 -0700

    Update chat/move requests to new API
    Also separates monolithic message handler into several specialized game
    event handlers. Handlers are organized into new data member m_Handlers
    and called using pointer to member function.

--- src/ui/lobbydialog.cpp
+++ src/ui/lobbydialog.cpp
@@ -19,18 +19,23 @@ LobbyDialog::LobbyDialog(const QString& gameName, QWidget *parent) :
     m_Scene(0)
 {
     ui->setupUi(this);
+
+    // Configure handlers list
+    m_Handlers["joined"] = joined;
+    m_Handlers["left"] = left;
+    m_Handlers["chatted"] = chatted;
+    m_Handlers["moved"] = moved;
+
     m_Scene = new QGraphicsScene(0, 0, SQ_SIZE * SQ_ROWS, SQ_SIZE * SQ_COLS);
 
     // Set up JSON-RPC parameters
-    QJsonArray connectParams;
     QJsonObject userObj;
     userObj.insert("authToken", User::instance().authToken());
     QJsonObject gameObj;
     gameObj.insert("name", gameName);
-    connectParams.append(userObj);
-    connectParams.append(gameObj);
+    QJsonArray connectParams = QJsonArray() << userObj << gameObj;
 
-    QJsonRpcMessage request = QJsonRpcMessage::createRequest("game.connect", connectParams);
+    QJsonRpcMessage request = QJsonRpcMessage::createRequest("Game.join", connectParams);
     m_Client.openBlocking();
     m_Client.sendMessage(request);
     connect(&m_Client, SIGNAL(messageReceived(QJsonRpcMessage)),
@@ -50,47 +55,54 @@ LobbyDialog::~LobbyDialog()
 void LobbyDialog::parseLobbyMessage(const QJsonRpcMessage& message)
 {
     qDebug() << message << message.method();
-    if (!message.method().compare("chatted"))
-    {
-        if (message.result().isObject())
-        {
-            QJsonObject obj = message.result().toObject();
-            QString user = obj["user"].toString();
-            QString text = obj["text"].toString();
-            QString log = QString("%0: %1").arg(user).arg(text);
-            ui->teHistory->insertPlainText(log);
-        }
-        QJsonArray params = message.params().toArray();
-        QString msg = QString("%0:%1").arg(params[0].toString()).arg(params[1].toString());
-        ui->teHistory->append(msg);
-    }
-    else if(!message.method().compare("moved"))
-    {
-        QJsonArray params = message.params().toArray();
-        QJsonObject userObj = params[0].toObject();
-        QJsonObject unitObj = params[1].toObject();
-        QJsonObject moveObj = params[2].toObject();
 
-        foreach(QGraphicsItem* item, m_Scene->items())
-        {
-            if (item->data(0).toInt() == unitObj["id"].toInt())
-            {
-                item->setPos(QPointF(moveObj["x"].toDouble(),
-                                     moveObj["y"].toDouble()));
-                break;
-            }
-        }
+    // Call the method appropriate for handling the message
+    (this->*m_Handlers[message.method()])(message);
+}
 
-        qDebug() << message.result().toString();
-    }
-    else if(!message.method().compare("joined"))
+void LobbyDialog::joined(const QJsonRpcMessage &message)
+{
+    qDebug() << message.result().toString();
+}
+
+void LobbyDialog::left(const QJsonRpcMessage &message)
+{
+    qDebug() << message.result().toString();
+}
+
+void LobbyDialog::chatted(const QJsonRpcMessage &message)
+{
+    if (message.result().isObject())
     {
-        qDebug() << message.result().toString();
+        QJsonObject obj = message.result().toObject();
+        QString user = obj["user"].toString();
+        QString text = obj["text"].toString();
+        QString log = QString("%0: %1").arg(user).arg(text);
+        ui->teHistory->insertPlainText(log);
     }
-    else if(!message.method().compare("left"))
+    QJsonArray params = message.params().toArray();
+    QString msg = QString("%0:%1").arg(params[0].toString()).arg(params[1].toString());
+    ui->teHistory->append(msg);
+}
+
+void LobbyDialog::moved(const QJsonRpcMessage &message)
+{
+    QJsonArray params = message.params().toArray();
+    QJsonObject userObj = params[0].toObject();
+    QJsonObject unitObj = params[1].toObject();
+    QJsonObject moveObj = params[2].toObject();
+
+    foreach(QGraphicsItem* item, m_Scene->items())
     {
-        qDebug() << message.result().toString();
+        if (item->data(0).toInt() == unitObj["id"].toInt())
+        {
+            item->setPos(QPointF(moveObj["x"].toDouble(),
+                                 moveObj["y"].toDouble()));
+            break;
+        }
     }
+
+    qDebug() << message.result().toString();
 }
 
 void LobbyDialog::sendMoved(int id, const QPointF pos)
@@ -106,7 +118,7 @@ void LobbyDialog::sendMoved(int id, const QPointF pos)
     moveObj.insert("y", pos.y());
     QJsonArray moveParams = QJsonArray() << userObj << gameObj << unitObj << moveObj;
 
-    QJsonRpcMessage message = QJsonRpcMessage::createRequest("game.move", moveParams);
+    QJsonRpcMessage message = QJsonRpcMessage::createRequest("Game.move", moveParams);
     m_Client.sendMessage(message);
 }
 
@@ -122,6 +134,6 @@ void LobbyDialog::on_pbSend_clicked()
     ui->leMessage->clear();
     QJsonArray chatParams = QJsonArray() << userObj << gameObj << messageObj;
 
-    QJsonRpcMessage message = QJsonRpcMessage::createRequest("game.chat", chatParams);
+    QJsonRpcMessage message = QJsonRpcMessage::createRequest("Game.chat", chatParams);
     m_Client.sendMessage(message);
 }
--- src/ui/lobbydialog.h
+++ src/ui/lobbydialog.h
@@ -5,6 +5,7 @@
 #include "../jsonrpc/jsonrpcwebsocketclient.h"
 #include <QGraphicsScene>
 #include <QGraphicsItem>
+#include <QHash>
 
 namespace Ui {
 class LobbyDialog;
@@ -20,6 +21,10 @@ public:
 
 private slots:
     void parseLobbyMessage(const QJsonRpcMessage& message);
+    void joined(const QJsonRpcMessage& message);
+    void left(const QJsonRpcMessage& message);
+    void chatted(const QJsonRpcMessage& message);
+    void moved(const QJsonRpcMessage& message);
     void sendMoved(int id, const QPointF pos);
     void on_pbSend_clicked();
 
@@ -28,6 +33,7 @@ private:
     JsonRpcWebSocketClient m_Client;
     QString m_GameName;
     QGraphicsScene *m_Scene;
+    QHash<QString, void(LobbyDialog::*)(const QJsonRpcMessage&)> m_Handlers;
 };
 
 class MyPixmapItem : public QObject, public QGraphicsPixmapItem
